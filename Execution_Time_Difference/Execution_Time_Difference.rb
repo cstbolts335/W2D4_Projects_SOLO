1eb236605a0a8f794b830fb11f600d3b4fb86ea5
Chris Tompkins <cstbolts335@gmail.com>
2018-10-04 11:30:16 -0400

    Add Ruby File Called Execution Time Difference

---
+++ Execution_Time_Difference/Execution_Time_Difference.rb
@@ -0,0 +1,13 @@
+  # Phase 1: my_min
+  # list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+
+  #First, write a function that compares each element to every other element of the list. Return the element if all other elements in the array are larger.
+  #What is the time complexity for this function?
+
+  def check_list(array)
+    min = array.first
+    array.each do |num|
+      min = num if num < min
+    end
+    min
+  end

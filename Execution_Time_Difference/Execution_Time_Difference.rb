bc088c0319c02c50884bd50cf6e760f9775a3470
Chris Tompkins <cstbolts335@gmail.com>
2018-10-04 11:31:16 -0400

    First Answer Added

--- Execution_Time_Difference/Execution_Time_Difference.rb
+++ Execution_Time_Difference/Execution_Time_Difference.rb
@@ -4,6 +4,7 @@
   #First, write a function that compares each element to every other element of the list. Return the element if all other elements in the array are larger.
   #What is the time complexity for this function?
 
+  # 
   def check_list(array)
     min = array.first
     array.each do |num|
